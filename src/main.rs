d2f56065a56ad788c3460f1f03f3d70413a59892
npty <nptytn2@gmail.com>
2023-03-06 21:47:13 +0700

    chore: add table and vscode command

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ use file_info::file_info::FileInfo;
 use filters::filters::{is_hidden, is_node_modules};
 use path::CustomPath;
 use std::fs::{self};
+use std::process::Command;
+
+#[macro_use]
+extern crate prettytable;
+use prettytable::Table;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,6 +23,9 @@ pub struct Args {
 
     #[arg(short, long, default_value_t = false)]
     pub display_date: bool,
+
+    #[arg(short, long, default_value_t = false)]
+    pub vs_code: bool,
 }
 
 const MAX_DEPTH: usize = 5;
@@ -26,7 +34,7 @@ fn main() {
     let args = Args::parse();
     let path = dirs::home_dir().unwrap().join("workspace");
 
-    let mut data: Vec<FileInfo> = fs::read_dir(path)
+    let mut data: Vec<FileInfo> = fs::read_dir(&path)
         .unwrap()
         .map(|entry| {
             let entry = entry.unwrap();
@@ -43,12 +51,30 @@ fn main() {
     data.sort_by_key(|file_info| file_info.modified);
     data.reverse();
 
-    data.iter().take(args.size.into()).for_each(|file_info| {
-        let mut date = String::new();
-        if args.display_date {
-            let datetime: DateTime<Local> = file_info.modified.into();
-            date = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
-        }
-        println!("{:?} {}", file_info.name, date);
-    });
+    let mut table = Table::new();
+    if args.display_date {
+        table.set_titles(row!["Name", "Modified"]);
+    } else {
+        table.set_titles(row!["Name"]);
+    }
+
+    if !args.vs_code {
+        data.iter().take(args.size.into()).for_each(|file_info| {
+            if args.display_date {
+                let datetime: DateTime<Local> = file_info.modified.into();
+                let date = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+                table.add_row(row![file_info.name, date]);
+            } else {
+                table.add_row(row![file_info.name]);
+            }
+        });
+        table.printstd();
+    } else {
+        data.iter().take(args.size.into()).for_each(|file_info| {
+            Command::new("code")
+                .arg(path.join(&file_info.name))
+                .spawn()
+                .expect("Failed to open file in VS Code");
+        });
+    }
 }
