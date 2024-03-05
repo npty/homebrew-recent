42402cd9a2205e88f25054fddcfd124dfcdc7402
npty <nptytn2@gmail.com>
2024-03-05 15:55:27 +0700

    chore: update tag

--- Formula/recent.rb
+++ Formula/recent.rb
@@ -1,7 +1,7 @@
 class Recent < Formula
   desc "CLI for listing recently modified files"
   homepage "https://github.com/npty/homebrew-recent"
-  url "https://github.com/npty/homebrew-recent/releases/download/0.1.0/recent-0.1.0-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/npty/homebrew-recent/releases/download/v0.1.0/recent-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "26628e0d1e2b63b39d343e37b90d6cd04b3a2c831c2dfb304be31914f5d94fc0"
   version "0.1.0"
 
