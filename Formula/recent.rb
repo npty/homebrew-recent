a28b1b8505a98c07817661ac88b8094d970c58e2
npty <nptytn2@gmail.com>
2024-03-05 15:54:50 +0700

    chore: update sha256

--- Formula/recent.rb
+++ Formula/recent.rb
@@ -2,7 +2,7 @@ class Recent < Formula
   desc "CLI for listing recently modified files"
   homepage "https://github.com/npty/homebrew-recent"
   url "https://github.com/npty/homebrew-recent/releases/download/0.1.0/recent-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "4c6b05187a2701047922cec2f91e96069bbe935e"
+  sha256 "26628e0d1e2b63b39d343e37b90d6cd04b3a2c831c2dfb304be31914f5d94fc0"
   version "0.1.0"
 
   def install
